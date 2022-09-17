d50a520d46aa7fa0ec5ec5f1816a5ba486c1533a
Andy <moyanoandres1@gmail.com>
2022-09-17 14:13:18 +0200

    added regexp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "SPI.h"
 #include "RTClib.h"
 
+#include <Regexp.h>
+
 #include <FS.h>
 #include <LittleFS.h>
 #include <ESP8266WiFi.h>
@@ -25,22 +27,102 @@ bool state = false;
 const char *ssid_AP = "ESP8266_AP";
 const char *password_AP = "12345678";
 
-// const char* ssid = "Zurimuri"; //Replace with your own SSID
-// const char* password = "sogtulakk"; //Replace with your own password
-
 AsyncWebServer server(80);
-//ESP8266WebServer server(80);
 
-// Comprueba si esta programado el encendido
-bool isScheduledON(DateTime date)
+
+String horarios[5]; // creo array de horarios
+float horariosFloat[5];
+int contador = 0;
+
+
+/**
+ * Enciende la bomba
+ * 
+ * */
+bool enciendeBomba(DateTime date)
 {
   int weekDay = date.dayOfTheWeek(); // Domingo 0 / Lunes 1 / Martes 2 / Miercoles 3 / Jueves 4 / Viernes 5 / Sabado 6
   float hours = date.hour() + date.minute() / 60.0;
-  bool hourCondition = (hours > 14.00 && hours < 14.05) || (hours > 21.00 && hours < 23.00);
+  Serial.println(hours);
   bool dayCondition = (weekDay == 5 || weekDay == 6 || weekDay == 0);
+  bool hourCondition;
 
-  return (hourCondition && dayCondition) ? true : false;
+  for (int i = 0; i< contador;i++){
+    hourCondition += (hours >= horariosFloat[i] && hours <= horariosFloat[i] + .05);
+  }
+  
+  bool condicionhoraria = (hours >= 14.09);
+
+  //return (condicionhoraria && dayCondition) ? true : false;
+  return (condicionhoraria) ? true : false;
 }
+
+void compilePattern(){
+
+  MatchState ms;
+  ms.Target ("Testing: answer=42"); // String to be searched
+
+  /**
+   * Regexp: 
+   * 1. Hours 
+   * 2. Minutes to finish
+   * 3. Days at week
+   * */
+  String regExp = "14.00,21.00;5;1,4,5";
+
+  String matcher = ("[]");
+
+
+}
+
+// called for each match
+void setHorarios  (const char * match,          // matching string (not null-terminated)
+                      const unsigned int length,   // length of matching string
+                      const MatchState & ms)      // MatchState in use (to get captures)
+{
+
+  String hora;
+  for (int i = 0; i< length ; i++){
+    hora += match[i];
+  }
+
+  horariosFloat[contador] = hora.toFloat();
+  contador++;
+
+}  // end of match_callback 
+
+
+
+void pruebaSetup(){
+  unsigned long count;
+  char buf [100] =  "14.00,13.44;5;1,4,5";
+
+  
+  MatchState ms (buf); // match state object
+
+
+  // // GlobalMatch itera sobre un pattern, llama al callback cada vez que encuentra lo 
+  // // que busca y luego devuelve la cantidad de veces que encontro eso
+  count = ms.GlobalMatch(("(%d%d).(%d%d)"), setHorarios);
+  //                       // "[o%a]"
+  //                       // "^[0-9]{2}.[0-9]{2}"
+  //                       // "(o%a+)( )"
+
+  // char result = ms.Match(("(%d%d).(%d%d)(%p)"),0);
+  // if (result == REGEXP_MATCHED){
+  //   String hola = ms.GetMatch(buf);
+  //   Serial.print("esto devuelve getmatch: ");
+  //   Serial.println(hola);
+  // }
+
+  // show results
+  Serial.print (count );
+  Serial.println(" matches. ");
+}
+
+
+
+
 
 void printDate(DateTime date)
 {
@@ -81,29 +163,25 @@ void setup()
   Serial.begin(9600);
   delay(1000);
 
+  pruebaSetup();
+  for(int i = 0; i< contador ; i++){
+    Serial.print("Horario: ");
+    Serial.println(horariosFloat[i]);
+  }
+
   if (!rtc.begin())
   {
     Serial.println(F("Couldn't find RTC"));
     while (1);
   }
 
-  // Initialize SPIFFS
-  LittleFS.begin();
 
+  LittleFS.begin();
   WiFi.mode(WIFI_AP);
   //WiFi.softAP(ssid_AP, password_AP);
   WiFi.softAP(ssid_AP);
   Serial.print("AP dirección IP: ");
   Serial.println(WiFi.softAPIP());
-
-  // WiFi.begin(ssid, password);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(1000);
-  //   Serial.println("Connecting to WiFi..");
-  // }
-  // Serial.println(WiFi.localIP());
-
-
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/index.html",String(), false, processor);
@@ -116,7 +194,6 @@ void setup()
   });
   
   server.begin();
-
   Serial.println("Servidor inicializado.");
 
 
@@ -140,20 +217,17 @@ void loop()
   DateTime now = rtc.now(); // Obtener fecha y hora actual
   //server.handleClient();
 
-  if (!state && isScheduledON(now))
-  {
-    digitalWrite(rele, HIGH);
+  if (!state && enciendeBomba(now)){
+    digitalWrite(rele, LOW);
     state = true;
     Serial.println("Activado");
-  }
-  else if (state && !isScheduledON(now))
-  {
-    digitalWrite(rele, LOW);
+  } else if (state && !enciendeBomba(now)) {
+    digitalWrite(rele, HIGH);
     state = false;
     Serial.print("Desactivado");
   }
 
-  // printDate(now);
+  //printDate(now);
 
   delay(3000);
 }
